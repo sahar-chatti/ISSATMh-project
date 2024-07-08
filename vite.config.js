a08c33c687d2815063530cdcd17cf8047f881859
sahar-chatti <sahar.chatti@gmail.com>
2024-07-08 11:43:33 +0100

    vercel dep version

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist', // specify your output directory here
+      },
     plugins: [
         laravel({
             input: [
